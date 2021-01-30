3f96d5aa6599bcb1ed116699a71fb3c4a1e08206
Craig Drummond <craig.p.drummond@gmail.com>
2021-01-30 08:20:44 +0000

    Use orignal artist name when looking to match all genres

--- lib/app.py
+++ lib/app.py
@@ -247,7 +247,7 @@ def similar_api():
     for seed in seed_track_db_entries:
         accepted_tracks = 0
         first_sim = None
-        match_all_genres = ('ignoregenre' in cfg) and ('*'==cfg['ignoregenre'] or (seed['artist'] in cfg['ignoregenre']))
+        match_all_genres = ('ignoregenre' in cfg) and ('*'==cfg['ignoregenre'] or (seed['artist.orig'] in cfg['ignoregenre']))
 
         # Query DB for similar tracks
         resp = db.get_similar_tracks(seed, seed_genres, all_genres, min_duration, max_duration, skip_rows, match_all_genres)
