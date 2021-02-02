a94cc707838ed00c86deec37e789f846adb9c4a3
Craig Drummond <craig.p.drummond@gmail.com>
2021-02-02 20:41:58 +0000

    Set shuffle factor to 3

--- lib/app.py
+++ lib/app.py
@@ -25,8 +25,9 @@ MIN_TRACKS_TO_RETURN          = 5    # Min value for 'count' parameter
 MAX_TRACKS_TO_RETURN          = 50   # Max value for 'count' parameter
 NUM_PREV_TRACKS_FILTER_ARTIST = 15   # Try to ensure artist is not in previous N tracks
 NUM_PREV_TRACKS_FILTER_ALBUM  = 25   # Try to ensure album is not in previous N tracks
-SHUFFLE_FACTOR                = 4    # How many (shuffle_factor*count) tracks to shuffle?
+SHUFFLE_FACTOR                = 3    # How many (shuffle_factor*count) tracks to shuffle?
 VERBOSE_DEBUG                 = False
+
 
 class EssentiaApp(Flask):
     def init(self, args, app_config):
